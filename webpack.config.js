22411d4784045237b89f083b4a0eeb129407efa6
Yonatan <kra.yonatan@gmail.com>
2019-10-12 13:41:53 +0300

    Style, HTML and css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,25 @@ module.exports = {
         new CleanWebpackPlugin(['dist']),
         // create our index.html to view our app in the browser
         new HtmlWebpackPlugin({
-            title: 'Your Phrase Fireworks!'
+            title: 'Your Killer App!'
         })
     ],
+    module: {
+        rules: [
+            // use the html loader
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: {loader: 'html-loader'}
+            },
+            // use the css loaders (first load the css, then inject the style)
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 };
