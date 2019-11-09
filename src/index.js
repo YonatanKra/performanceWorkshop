40dff435557d8ff72d6202ec105b12917ef0a82c
Yonatan <kra.yonatan@gmail.com>
2019-11-09 15:11:11 +0200

    Add loading bar - same frame doesnt show

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import './search-bar/search-bar.component';
 import './data-table/data-table.component';
+import './loading-bar';
+
 const searchBar = document.createElement('search-bar');
 const dataTable = document.createElement('data-table');
+const progressBar = document.createElement('progress-bar');
+
+progressBar.style.position = 'fixed';
+progressBar.style.top = '200px';
 
 const queryServer = 'api/search';
 searchBar.setAttribute('data-url', queryServer);
 searchBar.addEventListener('query-response', e => {
+    document.body.appendChild(progressBar);
     dataTable.refreshData(e.detail);
 });
+
+dataTable.addEventListener('data-table-updated', () => {
+    document.body.removeChild(progressBar);
+});
+
 document.body.appendChild(searchBar);
 document.body.appendChild(dataTable);
 
