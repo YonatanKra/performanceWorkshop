da8a467adbf38d5dfa888386c3ef5e2ba2671d39
yonatankra <yonatan.kra@vonage.com>
2021-12-25 18:15:52 +0200

    good and bad searches

--- server/index.js
+++ server/index.js
@@ -2,14 +2,37 @@
 const express = require('express');
 const app = express();
 const faker = require('faker');
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
-const nResponses = 1000;
+let nResponses = 1000;
 
-function generateData() {
+function generateData(responses = nResponses) {
+    const fakeId = `${1}-${new Date().getTime()}`;
+    const fakeName = faker.name.firstName();
+    const fakeEmail = faker.internet.email();
+
+    let users = new Array(responses).fill(0).map(i => ({
+        id: fakeId, name: fakeName, email: fakeEmail
+    }));
+
+    for (let i = 0; i < responses; i++) {
+
+        let firstName = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        let email = faker.internet.email();
+
+        users[i]["id"] = `${i}-${new Date().getTime()}`;
+        users[i]["name"] = firstName + lastName;
+        users[i]["email"] = email;
+    }
+
+    return users;
+}
+
+function generateData2(responses = nResponses) {
     let users = [];
 
-    for (let i = 0; i < nResponses; i++) {
+    for (let i = 0; i < responses; i++) {
 
         let firstName = faker.name.firstName();
         let lastName = faker.name.lastName();
@@ -27,6 +50,15 @@ function generateData() {
 
 // routes will go here
 app.post('/api/search', function(req, res) {
+    res.send(generateData());
+    nResponses *= 10;
+});
+
+app.post('/api/bad-search', function(req, res) {
+    res.send(generateData2());
+});
+
+app.post('/api/good-search', function(req, res) {
     res.send(generateData());
 });
 
